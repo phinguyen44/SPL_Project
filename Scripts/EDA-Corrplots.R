0431970a658b46318926e2041735103597bbf955
phister <nguyen1.618@gmail.com>
2017-11-07 12:01:07 +0100

    added corrplots. work in progress

--- Scripts/EDA-Corrplots.R
+++ Scripts/EDA-Corrplots.R
@@ -17,8 +17,15 @@ wd = file.path(Sys.getenv("HOME"),"/Documents/Projects/SPL_Project")
 setwd(wd)
 
 source("Scripts/EDA.R")
-needs(PerformanceAnalytics)
+needs(PerformanceAnalytics, corrplot)
 
 #################################################################################################
 # Do corrplots
 
+# just for complete cases to check, remove factors
+df.slim.cc = df.slim[complete.cases(df.slim), ] %>% 
+  select(which(sapply(df.slim, function(x) !is.factor(x))))
+
+# IT DOESN'T LOOK PRETTY. Work on this.
+chart.Correlation(df.slim.cc)
+corrplot(cor(df.slim.cc), method = "number", type = "upper")
