a0226e3b0691a15cbbe3e489ac93c788cb85541c
Claudia <claudiaguenther@ninjapower.fritz.box>
2017-12-01 16:15:23 +0100

    Details to Q2

---
+++ Scripts/SummaryStatistics.R
@@ -0,0 +1,15 @@
+################################################################################
+# SummaryStatistics.R
+#
+################################################################################
+# Description:
+# Summary statistics on health and labour force participation across countries
+# 
+################################################################################
+
+################################################################################
+
+# Comment: How should we start this quantlet? Load the pre-processd data set?
+# Specify with data must be loaded? 
+
+
