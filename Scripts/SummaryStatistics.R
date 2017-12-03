3c8ec0d815618b3b2598a7494f62488f0a52616d
Claudia <claudiaguenther@ninjapower.fritz.box>
2017-12-03 15:04:58 +0100

    Changes in Q2, output tables

--- Scripts/SummaryStatistics.R
+++ Scripts/SummaryStatistics.R
@@ -45,63 +45,74 @@ lapply(neededPackages, library, character.only = TRUE)
 
 
 # Function for calculation group percentages per country
-group.percentage     = function(x){tapply(X = x, INDEX = df.out$country, 
+group.percentage     = function(y){tapply(X = y, INDEX = df.out$country, 
                       FUN = function(x){
-                        val = sum(x)/length(x)
+                        val = percent(sum(x)/length(x))
                         return(val)
                       })} 
 
-# Creates index vector for working woman and men (half or full time)
-IDX_w_l = ifelse({df.out$gender=="FEMALE"} & {df.out$labor_ft | df.out$labor_pt}, TRUE, FALSE)
-IDX_m_l = ifelse({df.out$gender=="MALE"}   & {df.out$labor_ft | df.out$labor_pt}, TRUE, FALSE)
-k = list(levels(df.out$country))
+
+# TODO: why is this not return a percentage entry?
 
 # Function for calculating labor particpation rate per country
-labor.part.percentage = function(x, i){tapply(X = x, INDEX = df.out$country, 
-                                          FUN = function(x, i){ # does no recognize index i
-                                              working = sum(ifelse(i, 1, 0))
-                                              all     = length(x)
-                                              val = working/all
-                                            return(val)
-                                          })}
+labor.part.share = by(df.out, list(df.out$country), function(z){
+  
+  # Create Index Vector seperated by gender
+  IDX_w_l = ifelse({z$gender=="FEMALE"} & {z$labor_ft | z$labor_pt}, TRUE, FALSE)
+  IDX_m_l = ifelse({z$gender=="MALE"}   & {z$labor_ft | z$labor_pt}, TRUE, FALSE)
+  
+  # Count 
+  working_w = sum(ifelse(IDX_w_l, 1, 0))
+  working_m = sum(ifelse(IDX_m_l, 1, 0))
+  
+  # Get Length in order to calculate percentages
+  len_w = length(IDX_w_l)
+  len_m = length(IDX_m_l)
+  
+  # Calculate percentages
+  perc_w = working_w / len_w
+  perc_m = working_m / len_m
+  
+  # Create Names
+  output.list = list(perc_w, perc_m)
+  names(output.list) = c(paste0(c("Female", "Male"), " Labor Participation Share"))
+  
+  return(output.list)
+  
+})
 
-# there is an error in the function: not returning labor rate but age percentage
-
+labor.part.share.df = do.call(rbind.data.frame, labor.part.share)
+names(labor.part.share.df) = c(paste0(c("Female", "Male"), " Labor Participation Share"))
 
 # Creating summary statistics dataframe with percentage entries/mean
-sum.stats = 
-      data.frame(matrix(nrow = 11, ncol = 0)) %>% 
-      mutate(observation  = summary(df.out$country),
-             age50_54_p   =   group.percentage(df.out$age50_54),
-             age55_59_p   =   group.percentage(df.out$age55_59),
-             age60_64_p   =   group.percentage(df.out$age60_64),
-             age50_54_n   =   age50_54_p * observation, 
-             age55_59_n   =   age55_59_p * observation,
-             age60_64_n   =   age60_64_p * observation,
-             h_chronic_p  =   group.percentage(df.out$h_chronic),
-             h_maxgrip_p  =   group.percentage(df.out$h_maxgrip),
-             h_adla_p     =   group.percentage(df.out$h_adla),
-             h_overweigh_p=   group.percentage(df.out$h_overweight),  
-             h_obese_p    =   group.percentage(df.out$h_obese),  
-             h_badment_p  =   group.percentage(df.out$h_badmental),  
-             h_goodsp_p   =   group.percentage(df.out$h_goodsp),
-             age50_54_ml_p=   labor.part.percentage(df.out$age50_54, IDX_m_l),
-             age55_59_ml_p=   labor.part.percentage(df.out$age55_59, IDX_m_l),
-             age60_64_ml_p=   labor.part.percentage(df.out$age60_64, IDX_m_l),
-             age50_54_wl_p=   labor.part.percentage(df.out$age50_54, IDX_w_l),
-             age55_59_wl_p=   labor.part.percentage(df.out$age55_59, IDX_w_l),
-             age60_64_wl_p=   labor.part.percentage(df.out$age60_64, IDX_w_l))
-
+sum.stats = data.frame(
+          cbind(labor.part.share.df, matrix(nrow = 11, ncol = 0)) %>% 
+                mutate(observation  = summary(df.out$country),
+                       age50_54_p   =   group.percentage(df.out$age50_54),
+                       age55_59_p   =   group.percentage(df.out$age55_59),
+                       age60_64_p   =   group.percentage(df.out$age60_64),
+                       age50_54_n   =   age50_54_p * observation, 
+                       age55_59_n   =   age55_59_p * observation,
+                       age60_64_n   =   age60_64_p * observation,
+                       h_chronic_p  =   group.percentage(df.out$h_chronic),
+                       h_maxgrip_p  =   group.percentage(df.out$h_maxgrip),
+                       h_adla_p     =   group.percentage(df.out$h_adla),
+                       h_overweigh_p=   group.percentage(df.out$h_overweight),  
+                       h_obese_p    =   group.percentage(df.out$h_obese),  
+                       h_badment_p  =   group.percentage(df.out$h_badmental),  
+                       h_goodsp_p   =   group.percentage(df.out$h_goodsp)))
 rownames(sum.stats) = levels(df.out$country)
+
+names(sum.stats) = c(paste0(c("Female", "Male"), " Labor Participation Share"),
+                     "Oberservations",
+                     paste0("Age ", rep(c("50-54", "55-59", "60-64"), 2), c(rep(" ", 3), rep(" Obs"), 3)),
+                     paste0(c("Chronic diseases", "ADLs", "Max. grip strength"), (" (mean)")),
+                     paste0(c("Overweight", "Obese", " Bad mental health", "Good self-perceived health"),
+                            " (in %)"))
 # Comment: Can we improve creating summary statistics by looping over certain variables?
 # Comment: Adla percentages are way too low compared to article!!!
 # Comment: labour participation rates are way to low!!! I think this has to do with the
 # assumption that 0 hours are interpreted as not working
-
-
-
-
-
 
 
 ################################################################################
@@ -113,4 +124,55 @@ rownames(sum.stats) = levels(df.out$country)
 k = function(y){formattable(x = y)}  
 
 k(sum.stats[,1:4])
+
+
+DF <- data.frame(Ticker=c("", "", "", "IBM", "AAPL", "MSFT"),
+                 Name=c("Dow Jones", "S&P 500", "Technology", 
+                        "IBM", "Apple", "Microsoft"),
+                 Value=accounting(c(15988.08, 1880.33, NA, 
+                                    130.00, 97.05, 50.99)),
+                 Change=percent(c(-0.0239, -0.0216, 0.021, 
+                                  -0.0219, -0.0248, -0.0399)))
+
+formattable(DF, list(
+  Name=formatter(
+    "row ",
+    style = x ~ ifelse(x == "Technology", 
+                       style(font.weight = "bold"), NA)),
+  Value = color_tile("white", "green"),
+  Change = formatter(
+    "span",
+    style = x ~ style(color = ifelse(x < 0 , "red", "green")),
+    x ~ icontext(ifelse(x < 0, "arrow-down", "arrow-up"), x)))
+)
+
+# Creates a formatter function showing above mean values on bold
+above_mean_bold = formatter("span", 
+                            style = x ~ style("font-weight" = ifelse(x > mean(x), "bold", NA)))
+
+formattable(DF1, list(
+  Observations = sign_formatter
+            ))
+
+# my formatting function
+above_mean_bold = formatter("span", 
+                            style = x ~ style("font-weight" = ifelse(x > mean(x), "bold", NA)))
+DF1 = data.frame(sum.stats[3:5])
+
+formattable(DF1, list(
+  Observations = above_mean_bold
+))
+
+# not working
+
+# formatting with inbuilt function
+formattable(DF1, list(
+  Observations = sign_formatter
+))
+
+# also not working: 
+#Fehler in create_obj(x, "formattable", list(formatter = formatter, format = list(...),  : 
+                                              #Objekt 'sign_formatter' nicht gefunden
+
+
 
