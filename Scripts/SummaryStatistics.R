f28e46fc4991e2648b55f8ddc724880b36e8643f
Claudia <claudiaguenther@ninjapower.fritz.box>
2017-12-04 09:35:53 +0100

    export output table

--- Scripts/SummaryStatistics.R
+++ Scripts/SummaryStatistics.R
@@ -155,4 +155,17 @@ sum.stats.out(DF1)
 # Show percentage entries
 # Show name of countries
 
+export_formattable <- function(f, file, width = "100%", height = NULL, 
+                               background = "white", delay = 0.2)
+{
+  w <- as.htmlwidget(f, width = width, height = height)
+  path <- html_print(w, background = background, viewer = NULL)
+  url <- paste0("file:///", gsub("\\\\", "/", normalizePath(path)))
+  webshot(url,
+          file = file,
+          selector = ".formattable_widget",
+          delay = delay)
+}
+
+export_formattable(sum.stats.out(DF1))
 
