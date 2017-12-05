8978fb033bc676126fdbd320e5abb4ca4dea9e05
Claudia <claudiaguenther@ninjapower.fritz.box>
2017-12-05 17:39:46 +0100

    Updated Wald test from aod package

--- Scripts/Wald.Test.R
+++ Scripts/Wald.Test.R
@@ -1,5 +1,5 @@
 ################################################################################
-# Probit.Regression
+# Wald.Test
 #
 ################################################################################
 # Description:
@@ -20,7 +20,7 @@ rm(list= ls()[!(ls() %in% c("df.reg", "df.splits"))])
 # LOAD NECESSARY PACKAGES & DATA
 
 # List all packages needed for session
-neededPackages = c("aod", "devtools")
+neededPackages = c("aod")
 allPackages    = c(neededPackages %in% installed.packages()[,"Package"]) 
 
 # Install packages (if not already installed) 
@@ -32,6 +32,7 @@ if(!all(allPackages)) {
 
 # Load all defined packages
 lapply(neededPackages, library, character.only = TRUE)
+
 
 ################################################################################
 
@@ -60,12 +61,10 @@ for(i in 1:length(allModels)){
   # Get Element
   ModelElement = allModels[[i]]
   
-  # Specify the of coefficients to be tested: only health variable
-  health = c(16:19)
+  # Specify number of coefficients: Columns - 1 (dependent Variable)
+  nTerms = ncol(ModelElement$data) - 1
   
-  # Test only the joint significance of health variables
-  # TODO: select health coefficients in a more efficient manner -> some Models have less coefficients
-  testOutput = try(wald.test(b = coef(ModelElement), Sigma = vcov(ModelElement), Terms = health)$result)
+  testOutput = try(wald.test(ModelElement, terms = nTerms))
   
   if(class(testOutput) == "try-error"){
     
@@ -85,13 +84,56 @@ for(i in 1:length(allModels)){
   }
   
   rm(ModelElement) # clean up
-
+  
   next
   
 }
 
-wald.bound = t(as.data.frame(wald.log))
+wald.bound = do.call("rbind.data.frame", wald.log)
 modelNames = names(allModels)
 wald.df = data.frame(modelNames, wald.bound)
 
+############ Create our own Wald Test ###############
 
+############ First get results from waldtest packes for female Austrian ###########
+
+install.packages("aod")
+library(aod)
+library(numDeriv)
+
+## Multivariate Case
+# W = (theta_est - theta_zero)' [Var_est(theta_est)]^(-1)*theta_est - theta_zero)
+#   = (theta_est)' *[Var_est(theta_est)]^(-1)* (theta_est)
+
+
+# Example for Austrain women
+allSummaries$AUT.FEMALE
+
+theta_est_t= t(allSummaries$AUT.FEMALE$coefficients[16:19,1])
+theta_est  = allSummaries$AUT.FEMALE$coefficients[16:19,1]
+Var_est_theta_inv = solve(vcov(allSummaries$AUT.FEMALE)[16:19, 16:19])
+
+W = theta_est_t %*% Var_est_theta_inv %*% theta_est
+
+pchisq(W, df=23, lower.tail=FALSE)
+
+g = wald.test(b = coef(allModels$AUT.FEMALE), Sigma = vcov(allModels$AUT.FEMALE), Terms = 16:19)
+W 
+g$result
+# => results are equal :) 
+
+######### JUST SOME NOTES #########
+
+
+
+
+
+## Univariate Case
+# W = (theta_est - theta_zero)^2 / Var_est(theta_est)
+# Calculation Z value from coefficients and standard error
+# for H0: all b equal to zero: z = (theta_est - 0) / se 
+
+# Example
+#Z = 0.21302 / 0.45758  
+#p.value = 2*(1-pt(abs(Z),nrow(allModels$AUT.FEMALE$data)-ncol(allModels$AUT.FEMALE$data)))
+#results<-cbind(Z,p.value)
