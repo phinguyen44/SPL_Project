e104968ec835b8e7308b254fad18c3346771a3db
phister <nguyen1.618@gmail.com>
2017-12-11 17:29:03 +0100

    converted to function

--- Scripts/ReadAndClean.R
+++ Scripts/ReadAndClean.R
@@ -10,11 +10,12 @@
 #
 ################################################################################
 
-ReadAndClean <- function(dataset = "easySHARE_rel6_0_0.rda") {
+read.and.clean <- function(dataset = "easySHARE_rel6_0_0.rda") {
 
     # LOAD NECESSARY PACKAGES & DATA
     # List all packages needed for session
-    neededPackages = c("dplyr", "tidyr", "ggplot2", "magrittr", "countrycode")
+    neededPackages = c("dplyr", "tidyr", "ggplot2", 
+                       "magrittr","infuser", "countrycode")
     allPackages    = c(neededPackages %in% installed.packages()[,"Package"])
 
     # Install packages (if not already installed)
@@ -25,24 +26,32 @@ ReadAndClean <- function(dataset = "easySHARE_rel6_0_0.rda") {
     }
 
     # Load all defined packages
-    lapply(neededPackages, library, character.only = TRUE)
+    lapply(neededPackages, function(x) suppressPackageStartupMessages(
+        library(x, character.only = TRUE)))
 
     # Load dataset
+    cat("Loading data set...", sep = "\n")
     load(dataset)
+    
     dat.input = easySHARE_rel6_0_0
     rm(easySHARE_rel6_0_0)
+    
+    rows.dat.input = nrow(dat.input)
 
-    ##########################################################################
+    ############################################################################
     # ENCODE MISSING VALUES
 
     # Organize data.frame by selecting relevant variables
+    cat("Selecting values only from Wave 1 and between ages 50 and 64.", 
+        sep = "\n")
+    
     dat = dat.input %>%
-        filter(wave == "1" & (age <= 64 & age >= 50)) %>%      # wave 1 & age filter
-        select(wave, country_mod,                              # dataset details
-               female, age, isced1997_r, ch001_, mar_stat,     # demographic variables
-               chronic_mod, maxgrip, adla, bmi2, eurod, sphus, # health indicators
-               ep013_mod, ep005_)                              # labor (outcome var)
-
+        filter(wave == "1" & (age <= 64 & age >= 50)) %>%  # wave 1 & age filter
+        select(wave, country_mod,                          # dataset details
+               female, age, isced1997_r, ch001_, mar_stat, # demo variables
+               chronic_mod, maxgrip, adla, bmi2, eurod, sphus, # health indices
+               ep013_mod, ep005_)                          # labor (outcome var)
+    
     rm(dat.input)
 
     # Encode missing values according to SHARE dataset guidelines
@@ -63,12 +72,14 @@ ReadAndClean <- function(dataset = "easySHARE_rel6_0_0.rda") {
 
     df = data.frame(df.decl)
 
-    # If working hours is NA and job status is not employed, this means individuals don't work
-    # If working hours is NA and job status is employed, we assume part time work
+    # If working hours is NA and job status is not employed, this means 
+    # individuals don't work
+    # If working hours is NA and job status is employed, we assume part time 
+    # work
     df$ep013_mod[is.na(df$ep013_mod) & df$ep005_ != 2] = 0
     df$ep013_mod[is.na(df$ep013_mod) & df$ep005_ == 2] = 1
 
-    ##########################################################################
+    ############################################################################
     # CREATE DATA FRAMES FOR ANALYSIS AND ESTIMATION
 
     # Get country information from "countrycode" package
@@ -76,8 +87,10 @@ ReadAndClean <- function(dataset = "easySHARE_rel6_0_0.rda") {
                      "AUT", "CHE")
     country_data = with(countrycode_data, data.frame(iso3c, iso3n))
 
-    # Variables are cleaned, converted into human-readable naming conventions, and
-    # converted to dummies as described in the paper
+    # Variables are cleaned, converted into human-readable naming conventions, 
+    # and converted to dummies as described in the paper
+    cat("Removing missing values.", sep = "\n")
+    
     df.out       = df %>%
         dplyr::left_join(country_data, by = c("country_mod" = "iso3n")) %>%
         dplyr::filter(iso3c %in% country_list) %>%
@@ -114,6 +127,15 @@ ReadAndClean <- function(dataset = "easySHARE_rel6_0_0.rda") {
     # TODO: determine threshold for "severe" and "mild" conditions (currently we
     # just set it as numeric
 
+    rows.df.out    = nrow(df.out)
+    rows.remove    = rows.dat.input - rows.df.out
+    
+    cat(infuse("Rows removed:   {{rows.remove}}", rows.remove = rows.remove), 
+        sep = "\n")
+    cat(infuse("Rows remaining: {{rows.dat}}", rows.dat = rows.df.out), 
+        sep = "\n")
+    cat("", sep = "\n")
+
     # Create standardized variables for numeric data
     standardize = function(x) {
         mean = sum(x)/length(x)
@@ -128,27 +150,28 @@ ReadAndClean <- function(dataset = "easySHARE_rel6_0_0.rda") {
     idx = sapply(df.out, is.numeric)
     idx = seq(1:length(idx))[idx]
 
-    # Creating separate data set with standardized numeric variables for regression,
-    # then reselect variables as described in paper (e.g. self-reported health is
-    # removed)
+    # Creating separate data set with standardized numeric variables for 
+    # regression, then reselect variables as described in paper (e.g. self-           # reported health is removed)
     df.reg = df.out %>%
         mutate_at(.vars = vars(idx),
                   .funs = standardize) %>%
-        mutate(labor_participation = !labor_np) %>% # invert to get labor_part rate
+        mutate(labor_participation = !labor_np) %>% # invert to get labor_part
         select(country, gender, age,
                h_chronic, h_adla, h_obese, h_maxgrip,
                edu_second, edu_high, children, couple,
                labor_participation)
 
-    # Create a list of data frames by country and gender, to be used in regression
-    df.splits  = split(df.reg, f = list(df.reg$country, df.reg$gender), drop = TRUE)
+    # Create a list of data frames by country and gender, to be used in 
+    # regression
+    df.splits  = split(df.reg, f = list(df.reg$country, df.reg$gender), 
+                       drop = TRUE)
 
     # Create necessary dummary variables for regression
     dummify = function(data.frame) {
         data.frame = data.frame %>%
-            select(-country, -gender)                # remove country/gender
-        model      = ~ 0 + .                         # needed to remove intercept
-        new.df     = model.matrix(model, data.frame) # create dummies
+            select(-country, -gender)               # remove country/gender
+        model      = ~ 0 + .                        # needed to remove intercept
+        new.df     = model.matrix(model, data.frame)# create dummies
         new.df     = data.frame(new.df)
         return(new.df)
     }
@@ -156,7 +179,12 @@ ReadAndClean <- function(dataset = "easySHARE_rel6_0_0.rda") {
     df.splits = lapply(df.splits, dummify)
 
     # df.out is for analysis, df.reg and df.splits are for estimation
-    all.output = list(df.out = df.out, df.splits = df.splits, df.reg = df.reg)
+    all.output = list(df.out = df.out, df.reg = df.reg, df.splits = df.splits)
+    
+    cat("Final output is a list containing 3 data.frames:", sep = "\n")
+    cat("`df.out` is a data.frame containing variables for calculating summary statistics.", sep = "\n")
+    cat("`df.reg` is a data.frame containing standardized variables for regression.", sep = "\n")
+    cat("`df.splits` splits the regression data.frame into individual data.frames for each country/gender split. 22 data.frames are contained in this list.", sep = "\n")
 
     return(all.output)
 }
