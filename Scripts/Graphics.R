781af0944d1e632d2c8533256bbc583a34d91c26
phister <nguyen1.618@gmail.com>
2017-12-23 07:06:38 +0100

    initialize new graphics branch

---
+++ Scripts/Graphics.R
@@ -0,0 +1,20 @@
+################################################################################
+# Graphics.R
+#
+################################################################################
+# Description:
+# Create useful graphics (Quantlet 6)
+# 
+################################################################################
+
+# distribution of age and # of chronic health problems
+
+# correlation graphics (labor force participation vs. different health metrics)
+
+# chloropeth of labor participation rates (men & women) (across age groups)
+
+# chloropeth of each metric
+
+# labor force participation over time (compare waves)
+
+# distribution of age by country
