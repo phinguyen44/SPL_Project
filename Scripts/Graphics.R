94c083dbadc14ac5b133dfdbde4042e501d2b5c6
phister <nguyen1.618@gmail.com>
2018-02-27 00:21:40 +0100

    complete gender split section

--- Scripts/Graphics.R
+++ Scripts/Graphics.R
@@ -5,7 +5,9 @@
 # Description:
 # Functions designed to create useful graphics (quantlet 6)
 # 
-# xxxx
+# health.gridmap(xvar, facetting): generate a tile grid map by a variable in
+# dataset, facetted by either gender or age. if numeric than average is
+# calculated. if logical than % is calculated
 # 
 # health.distribution(xvar, gen, countries, remove.outliers): show distribution
 # of numeric variable in dataset, facetted by gender and country.
@@ -19,32 +21,49 @@
 
 # accept metric and facetting as input
 # facetting can take either 'gender' or 'age' as inputs
-
-# return error if numeric input not selected or if neither 'gender' or 'age' is inputted
+# return error if neither 'gender' or 'age' is inputted
 
 # output side by side grid maps
 
 health.gridmap = function(xvar, facetting) {
     
+    # List all packages needed for session
+    neededPackages = c("dplyr", "ggplot2", "countrycode", 
+                       "gridExtra", "infuser")
+    allPackages    = c(neededPackages %in% installed.packages()[ , "Package"]) 
+    
+    # Install packages (if not already installed) 
+    if(!all(allPackages)) {
+        missingIDX = which(allPackages == FALSE)
+        needed     = neededPackages[missingIDX]
+        lapply(needed, install.packages)
+    }
+    
+    # Load all defined packages (silently)
+    invisible(lapply(neededPackages, library, character.only = TRUE))
+    
     # first convert age to numeric, not factor variable
     df.out$age = as.numeric(levels(df.out$age)[df.out$age])
     
     # STOPPING CONDITIONS
-    if (!is.numeric(df.out[[xvar]])) stop("'xvar' must be numeric")
+    if (!is.numeric(df.out[[xvar]]) | !is.logical(df.out[[xvar]])) {
+        stop("'xvar' must be numeric or logical")
+    }
     if (!(facetting %in% c('gender', 'age'))) {
         stop("'facetting' variable must be either 'gender' or 'age'")
     }
     # ADD TILE GRID LAYOUT
     coordinates = structure(list(
-        Country = c("Albania", "Austria", "Belarus", "Belgium", "Bosnia and
-                    Herzegovina", "Bulgaria", "Croatia", "Czech Republic",
-                    "Denmark", "Estonia", "Finland", "France", "Germany",
-                    "Greece", "Hungary", "Iceland", "Ireland", "Italy", 
-                    "Latvia", "Lithuania", "Luxembourg", "Malta", "Montenegro",
-                    "Netherlands", "Norway", "Poland", "Portugal", "Republic of
-                    Moldova", "Romania", "Russian Federation", "Serbia",
-                    "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland",
-                    "The former Yugoslav Republic of Macedonia", "Ukraine",
+        Country = c("Albania", "Austria", "Belarus", "Belgium", 
+                    "Bosnia and Herzegovina", "Bulgaria", "Croatia", 
+                    "Czech Republic", "Denmark", "Estonia", "Finland", "France",
+                    "Germany", "Greece", "Hungary", "Iceland", "Ireland",
+                    "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta",
+                    "Montenegro", "Netherlands", "Norway", "Poland", "Portugal",
+                    "Republic of Moldova", "Romania", "Russian Federation", 
+                    "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", 
+                    "Switzerland", "The former Yugoslav Republic of Macedonia", 
+                    "Ukraine", 
                     "United Kingdom of Great Britain and Northern Ireland"
                     ), 
         X = c(6, 5, 7, 3, 6, 8, 5, 5, 5, 7, 7, 2, 5, 7, 6, 1, 1, 4, 8, 7, 3, 3,
@@ -53,18 +72,115 @@ health.gridmap = function(xvar, facetting) {
         Y = c(1, 4, 6, 6, 3, 3, 3, 5, 7, 8, 9, 5, 6, 1, 4, 9, 7, 4, 7, 7, 5, 3,
               2, 6, 9, 6, 4, 4, 4, 7, 3, 5, 3, 4, 9, 5, 2, 5, 7
               ), 
-        InSet = c(FALSE, TRUE, FALSE, TRUE, FALSE, FALSE, 
-                  FALSE, FALSE, TRUE, FALSE, FALSE, TRUE, TRUE,
-                  TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE,
-                  FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE,
-                  FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE,
-                  TRUE, TRUE, FALSE, FALSE, FALSE
+        InSet = c(FALSE, TRUE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, TRUE,
+                  FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, TRUE,
+                  FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE,
+                  FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE,
+                  FALSE, FALSE, FALSE
                   )
         ), 
         .Names = c("Country", "X", "Y", "InSet"), 
         row.names = c(NA, -39L), 
         class = c("tbl_df", "tbl", "data.frame")
     )
+    
+    # Get correct country information
+    codes       = countrycode_data
+    df.codes    = left_join(coordinates, codes, 
+                            by = c("Country" = "country.name.en")) %>% 
+        dplyr::select(Country, iso3c, X, Y) 
+    
+    # plot function to be used later
+    # TODO:FIX GRAPH RANGE and COLORS
+    plot_function = function(df) {
+        p = ggplot(data = df, aes(x = X, y = Y)) +
+            geom_tile(aes(fill = get(xvar), color = ""), 
+                      color = "white", size = 0.6) +
+            geom_text(aes(label = iso3c), color = "white") +
+            geom_text(aes(label = round(get(xvar), 2)), vjust = 2, 
+                      color = "white", size = 3.5, na.rm = TRUE) +
+            scale_fill_continuous(low = "#fdd49e", high = "#b30000", 
+                                  na.value = "#CCCCCC", limits = c(0,1)) + 
+            coord_fixed(ratio = 1) +
+            theme_minimal() +
+            theme(axis.line        = element_blank(),
+                  axis.text        = element_blank(),
+                  axis.title       = element_blank(),
+                  panel.background = element_blank(),
+                  panel.grid       = element_blank(),
+                  legend.position  = "bottom") +
+            theme(plot.title    = element_text(size=16),
+                  plot.subtitle = element_text(size=10, 
+                                               color = "#7F7F7F"))
+        return(p)
+    }
+    
+    # IF GENDER SELECTED:
+    if (facetting = 'gender') {
+        
+        # if logical than calculate percentage
+        if (is.logical(df.out[[xvar]])) {
+            
+            df.graph = df.out %>% 
+                dplyr::select(country, gender, `xvar`) %>% 
+                group_by(country, gender) %>% 
+                summarize(value = sum(get(xvar))/length(get(xvar)))
+            names(df.graph)[3] = xvar
+            
+            groupings = split(df.graph, df.graph$gender, drop = TRUE)
+            
+            # split by gender
+            df.m = df.graph %>% 
+                filter(gender == "MALE")
+            df.f = df.graph %>% 
+                filter(gender == "FEMALE")
+            df.m = suppressWarnings(
+                full_join(df.codes, df.m, c('Country' = 'country'))) %>%
+                dplyr::select(iso3c, X, Y, `xvar`)
+            df.f = suppressWarnings(
+                left_join(df.codes, df.f, c('Country' = 'country'))) %>%
+                dplyr::select(iso3c, X, Y, `xvar`)
+        }
+        
+        # if numeric than calculate average
+        if (is.numeric(df.out[[xvar]])) {
+            
+            df.graph = df.out %>% 
+                dplyr::select(country, gender, `xvar`) %>% 
+                group_by(country, gender) %>% 
+                summarize(value = mean(get(xvar)))
+            names(df.graph)[3] = xvar
+            
+            # split by gender
+            groupings = split(df.graph, df.graph$gender, drop = TRUE)
+        }
+        
+        # PLOT
+        output = lapply(list(df.m, df.f), plot_function)
+        output[[1]] = output[[1]] + 
+            labs(title    = infuse("Percentage of {{metric}} for {{split}}", 
+                                   metric = xvar, split = 'male'),
+                 fill     = xvar)
+        output[[2]] = output[[2]] + 
+            labs(title    = infuse("Percentage of {{metric}} for {{split}}", 
+                                   metric = xvar, split = 'female'),
+                 fill     = xvar)
+        output
+        
+    }
+    
+    # IF AGE SELECTED:
+    if (facetting = 'age') {
+        
+    }
+
+    
+    
+
+    
+    
+    
+}
 
     
 ###### VIEW DISTRIBUTION OF NUMERIC VARIABLES
@@ -79,6 +195,20 @@ health.distribution = function(xvar,
                                gen              = "all", 
                                countries        = "all", 
                                remove.outliers  = TRUE) {
+    
+    # List all packages needed for session
+    neededPackages = c("dplyr", "ggplot2", "scales")
+    allPackages    = c(neededPackages %in% installed.packages()[ , "Package"]) 
+    
+    # Install packages (if not already installed) 
+    if(!all(allPackages)) {
+        missingIDX = which(allPackages == FALSE)
+        needed     = neededPackages[missingIDX]
+        lapply(needed, install.packages)
+    }
+    
+    # Load all defined packages (silently)
+    suppressMessages(lapply(neededPackages, library, character.only = TRUE))
     
     # first convert age to numeric, not factor variable
     df.out$age = as.numeric(levels(df.out$age)[df.out$age])
