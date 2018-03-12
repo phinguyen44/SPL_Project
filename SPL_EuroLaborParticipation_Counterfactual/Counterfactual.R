53abc4c0e35927b68f55b727b526a151c556c5c2
phister <nguyen1.618@gmail.com>
2018-03-12 14:22:40 +0100

    reorganize and initialize directories for quantlets

---
+++ SPL_EuroLaborParticipation_Counterfactual/Counterfactual.R
@@ -0,0 +1,217 @@
+################################################################################
+# Counterfactual
+#
+################################################################################
+# Description:
+# 
+# 
+################################################################################
+
+################################################################################
+
+rm(list= ls())
+
+################################################################################
+# SOURCE DATA
+
+source("Scripts/ReadAndClean.R")
+datasets = read.and.clean(dataset = "easySHARE_rel6_0_0.rda")
+
+#Only keep relevant data sets
+df.splits = datasets$df.splits
+rm(datasets)
+
+
+################################################################################
+
+# Probit for each country and gender 
+allModels = lapply(df.splits, function(z){
+    
+    
+    model = glm(z$labor_participationTRUE ~ . -age50, family = binomial(link = "probit"), data = z)
+    
+    return(model)
+    
+})
+
+
+# Return summaries
+allSummaries = lapply(allModels, summary)
+
+#TODO: Use source file for model estimation
+
+################################################################################
+# Counterfactual Exercise
+
+# Calculate employment rate based on whole population with mean probability
+
+# Estimate current employment rate
+empl.rate       = function(model){
+    
+    # Select data from model
+    X               = model$data
+    
+    # Predict probability of being employed of all individuals
+    empl.probability = predict(object = model, newdata =  X, type = "response")
+    
+    # Calculate predicted current employment rate
+    empl.rate        = sum(empl.probability)  / length(empl.probability)
+    
+    return(empl.rate)
+}
+
+# Function for defining models with modified dataset (perfect health)
+X.cf   = function(model){
+    
+    # Select data from model
+    X                   = model$data
+    
+    #Create counterfactual data set with ideal health condition
+    X_cf                = X
+    
+    # Find ideal health conditions by finding the minimum value for each variable
+    X_min               = data.frame(t(apply(X, 2, min)))
+    
+    # Replace mean values by values representing no health issues
+    names.vec          = c("h_chronic", "h_adlaTRUE", "h_obeseTRUE")
+    X_cf[, names.vec]  = X_min[names.vec]
+    
+    # Create index vector for 50 and 51 year olds
+    age_50_51     = ifelse(X$age50 == 1 | X$age51 == 1, 1,0)
+    
+    # Calculate max grip mean of individuals age 50-51 and replace
+    X_cf[, c("h_maxgrip")] =  tapply(X$h_maxgrip, age_50_51 == 1, mean)[[2]]
+    
+    model$data   = X_cf
+    
+    return(model)
+}
+
+# Returns a list with all models with modified datasets
+allModels.cf = lapply(allModels, X.cf)
+
+# Calculate current and counterfactual employment rates
+empl.current     = lapply(allModels, empl.rate)
+empl.counterfact = lapply(allModels.cf, empl.rate)
+
+# Store results and display them
+employment            = data.frame(cbind(unlist(empl.current), unlist(empl.counterfact)))
+colnames(employment)  = c("Employment Current" , "Employment Counterf.") 
+
+################################################################################
+# Counterfactual exercise for each age group
+
+### Function for current and counterfactual employment rate among age groups
+# Default arguments: group size and lower bound of age groups (group.low)
+
+# Estimate current employment rate for age groups
+empl.rate.age       = function(model, group.size = 5, group.low = c(50,55,60)){
+    
+    #Initialize storage of results
+    empl.rate = numeric(3)
+    
+    # Select data from model
+    X               = model$data
+    
+    # Predict probability of being employed of all individuals
+    empl.probability = predict(object = model, newdata =  X, type = "response")
+    
+  # Create age group Index vectors
+    #Initialize result list containing age group index vectors
+    IND_row_vec = list()
+    
+    # Define vector of relevant age groups by youngest age
+        for (i in group.low){
+            
+                    # Define upper bound k of age group
+                    k = i + group.size -1
+                    
+                    # Create labels and contents of each age group
+                    vec.label = paste("names.vec.age", i, sep=".") 
+                    
+                    # Assign ages to the age groups
+                    z = assign(vec.label, paste0("age",i:k))
+                    
+         # For age groups of size 1, use simple indexing (cannot use apply)
+         if (length(z) == 1){
+            
+              IND_row_vec[[i]] = X[, z]
+        
+        # For all age groups greater than size 1      
+         } else {
+                    
+          # Sum over rows to find individuals belonging to age group i
+          IND_row_vec[[i]] = apply(X[, z], 1, sum)
+             }
+        }
+    
+    # Extract list of relevant age groups, which are each stored in a list 
+    IND_row_vec   = IND_row_vec[group.low]
+    
+
+ # Calculate predicted current employment rate for age groups
+    
+    # loop over age group list
+    for (k in 1: length(IND_row_vec)){
+        
+        empl.rate[k] = empl.probability %*% IND_row_vec[[k]] / sum(IND_row_vec[[k]])
+    }
+    
+    return(empl.rate)
+}
+
+# Calculate current and counterfactual employment rates for age groups
+empl.current.age     = t(data.frame(lapply(allModels, empl.rate.age)))
+empl.counterfact.age = t(data.frame(lapply(allModels.cf, empl.rate.age)))
+
+employment.age           = data.frame(cbind(empl.current.age, empl.counterfact.age))
+colnames(employment.age) = paste0(c(rep("empl.current", 3) , rep( "empl.counterfact", 3)), 
+                                  c("50.54","55.59", "60.64"))
+
+# Test for other age groups: Example of employment for each age group
+empl.rate.age(allModels$Germany.MALE, group.size = 1, group.low = c(50:63))  
+empl.rate.age(allModels.cf$Germany.MALE, group.size = 1, group.low = c(50:63))
+################################################################################
+
+# Decline in participation due to decline in health condition
+    
+health.decline = function(X){
+
+    # Differences between counterf. and current participation rates
+    dif.60.64 = X[,"empl.counterfact60.64"] - X[, "empl.current60.64"]
+    dif.50.54 = X[,"empl.counterfact50.54"] - X[, "empl.current50.54"]
+    
+    # Absolute difference in current participation rate
+    dif.abs   = X[,"empl.current50.54"] - X[,"empl.current60.64"]
+
+    measure   =  (dif.60.64 - dif.50.54) / dif.abs 
+   
+     return(measure)
+    
+    }
+
+participation.health = data.frame(health.decline(employment.age))
+
+
+################################################################################
+# Some Remarks
+
+# Define outout formate of counterfactual estimates: list, numeric, etc? 
+
+
+empl.each.age = list()
+
+# Calculate employment for each age for all countries
+for (i in 1:length(allModels)){
+    
+    k = allModels[[i]]
+    
+    empl.each.age[[i]] = empl.rate.age(k, group.size = 1, group.low = c(50:64))
+    
+}
+
+names(empl.each.age) = names(allModels)
+
+################################################################################
+
+
---
+++ SPL_EuroLaborParticipation_LoadWald/LoadWald.R
@@ -0,0 +1,123 @@
+################################################################################
+# Wald Test for joint significance
+
+# Note: This a Wald test for the joint signifance of a subset of model coefficients
+# m = number of restrictions
+# Beta is vector of coefficients of size k x 1 
+# Signf.level is the desired significance level (between 0 and 1). Default: 0.95
+# Spec is vector of integers of length 0 < m ≤ k specifying the subset of coefficients
+# to be jointly tested
+
+library("Matrix")
+
+joint.wald.test = function(model.summary, signf.level = 0.95, spec = NULL){
+  
+  # Set up test restrictions
+  if(is.null(spec)){
+    spec = 1:length(beta) # default joint is significance test
+  } else if(is.logical(spec)){ stop("spec cannot be a logical vector, tranform to interger vector!")
+  } else if(any(spec == 0)){ stop("spec cannot contain zero values!")
+  } else if(!is.integer(spec)){
+      spec.len = length(spec)
+      spec = as.integer(spec, length = spec.len)
+      warning("Converting spec to integer and proceeding")
+    }
+  
+    # Check Input
+    if(class(model.summary) != "summary.glm") stop("model.summary must be a glm summary!")
+    if(signf.level > 1 | signf.level < 0) stop("signf.level out of bounds!")
+    
+    # Define test elements
+    joint.wald.test        = numeric(6)
+    names(joint.wald.test) = c("Test","W","p-value", "df", "H0" , "Decision")
+    beta                   = model.summary$coefficients[,1]
+    Var_beta_est           = vcov(model.summary)
+    
+    # Wald test statistic
+    W = t(beta[spec]) %*% solve(Var_beta_est[spec,spec]) %*% beta[spec]
+    
+    # Set up test output
+    chi2               = qchisq(signf.level, df = length(spec))
+    pval               = 1-pchisq(W,length(spec))
+    joint.wald.test[1] = "Wald"
+    joint.wald.test[2] = format(   W, digits = 4) 
+    joint.wald.test[3] = format(pval, digits = 4)
+    joint.wald.test[4] = length(spec)
+    joint.wald.test[5] = "b equal to 0"
+    joint.wald.test[6] = ifelse(pval <= 1- signf.level, "Reject H0", "Cannot reject H0")
+    joint.wald.test
+}
+
+################################################################################
+## Wald Test for linear hypothesis
+
+# Note: This a general Wald test for linear hypothesis of model beta coefficients
+# m = number of restrictions
+# Beta is vector of coefficients of size k x 1 
+# Signf.level is the desired significance level (between 0 and 1). Default: 0.95
+# R is Jacobian matrix of size m x k 
+# r is restriction function size m x 1
+# Example: H0: b1 + b2 = 1 and 2*b1 = 0  (2 restrictions, m = 2)
+# R = 1 1 0 0 ...0      and r = 1
+#     2 0 0 0 ...0              0 
+
+# For Debug
+# model.summary = allSummaries[[1]] 
+
+general.wald.test = function(model.summary, signf.level = 0.95, R = NULL, r = NULL){
+    
+    # Check input
+    if(class(model.summary) != "summary.glm") stop("model.summary must be a glm summary!")
+    if(signf.level > 1 | signf.level < 0) stop("signf.level out of bounds!")
+  
+    # Define test elements
+    general.wald.test        = numeric(6) 
+    names(general.wald.test) = c("Test","W","p-value", "df", "H0" , "Decision")
+    beta                     = model.summary$coefficients[, 1]
+    Var_beta_est             = vcov(model.summary)
+ 
+    # Set up restriction matrix/vector for linear hypothesis
+    # default option is joint significants of all coefficients
+    if (is.null(R)){
+        R = diag(1, length(beta)) # default of R is identity matrix 
+    } 
+    
+    if (is.null(r)){
+        r = rep(0, length(beta)) # default for r is null vector
+    } 
+    
+    # Now R and rdefinitely exist, check dimensions
+    # R must be a matrix/dataframe/vector of size m x k, where m is the number of restrictions and k the number of all coefficients in the model summary.
+    
+    ## Testing R
+    dim_R = dim(R)
+    
+    # Columns
+    k = length(beta) # nrow(model.summary$coefficients)
+    
+    # Rows; general hypothesis testing, therefore m = length(r)
+    m = length(r)
+    
+    ##  Apply test
+    if(dim_R[1] != m | dim_R[2] != k) stop("R has wrong dimension!")
+    
+    # Check rank of R
+    if(rankMatrix(R)[1] != m) stop("R has wrong rank!")
+    
+    # Wald test statistic
+    W = t(R%*%beta - r) %*% solve(R%*% Var_beta_est %*% t(R)) %*% (R%*%beta - r)
+    
+    # Set up test output
+    chi2                 = qchisq(signf.level, df=length(r))
+    pval                 = 1-pchisq(W,length(r))
+    general.wald.test[1] = "Wald"
+    general.wald.test[2] = format(   W, digits = 4) 
+    general.wald.test[3] = format(pval, digits = 4)
+    general.wald.test[4] = length(r)
+    general.wald.test[5] = "R*b = r"
+    general.wald.test[6] = ifelse(pval <= 1- signf.level, "Reject H0", "Cannot reject H0")
+    general.wald.test
+}
+
+################################################################################
+
